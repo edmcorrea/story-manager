fd100ec512a713a2356bef75d752558f62d32aaf
Edmilson Corrêa <edm.correa@hotmail.com>
2022-10-18 18:48:19 -0300

    adding correction req4

--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -3,12 +3,10 @@ const { productSchema } = require('./schemas');
 const validateNewProduct = (name) => {
   const { error } = productSchema.validate(name);
 
-  const { type } = error.details[0];
-
-  if (error && type === 'any.required') {
+  if (error && error.details[0].type === 'any.required') {
     return { type: 'NAME_NOT_FOUND', message: '"name" is required' };
   }
-  if (error && type === 'string.min') {
+  if (error && error.details[0].type === 'string.min') {
     return {
       type: 'NAME_NOT_LENGTH',
       message: '"name" length must be at least 5 characters long',
